6a5fa3404906405c2974eb82f5a261cbdf8bf477
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-03-05 18:22:17 +0530

    Update element.js

--- src/components/element.js
+++ src/components/element.js
@@ -1,8 +1,6 @@
 import React from 'react';
 function Element(props) {
-  return {
-    <div className="element">{props.item}</div>;
-  }
+  return <div className="element">{props.item}</div>;
 }
 
 export default Element;
